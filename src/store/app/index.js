ab543ba5ffa00e1a957e45b1b97e3d7443d64b02
Sarman-Dasa <sarmand@zignuts.com>
2023-05-29 19:14:51 +0530

    add state variable and mutations for user

--- src/store/app/index.js
+++ src/store/app/index.js
@@ -5,6 +5,8 @@ export default {
   state: {
     windowWidth: 0,
     shallShowOverlay: false,
+    token:null,
+    userData:null
   },
   getters: {
     currentBreakPoint: state => {
@@ -23,6 +25,13 @@ export default {
     TOGGLE_OVERLAY(state, val) {
       state.shallShowOverlay = val !== undefined ? val : !state.shallShowOverlay
     },
+    UPDATE_AUTH_TOKEN(state, val) {
+        state.token = val;
+    },
+    UPDATE_AUTH_USER_DATA(state, val) {
+      state.userData = val;
+    }
+
   },
   actions: {},
 }
