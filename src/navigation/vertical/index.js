7b5144927d126323aff2337ff6e0a7a8b359cd41
Sarman-Dasa <sarmand@zignuts.com>
2023-06-05 16:02:13 +0530

    set sidebar menu base on user role

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -1,17 +1,36 @@
-export default [
+import store from "@/store";
+
+let userRole = store.state.app.userData.role;
+console.log("Role::",userRole);
+let menuItem = [
   {
     title: 'Home',
     route: 'home',
     icon: 'HomeIcon',
-  },
-  {
-    title: 'Second Page',
-    route: 'second-page',
-    icon: 'FileIcon',
-  },
-  {
-    title: 'Employee List',
-    route: 'employeeList',
-    icon: 'UserIcon'
   }
 ]
+
+let hrMenuItem = {
+  title: 'Second Page',
+  route: 'second-page',
+  icon: 'FileIcon',
+}
+
+let employeeItem = {
+  title: 'Employee List',
+  route: 'employeeList',
+  icon: 'UserIcon'
+}
+
+if(userRole == 'superAdmin') {
+  menuItem.push(hrMenuItem);
+  menuItem.push(employeeItem);
+}
+
+else if(userRole == 'humanResource') {
+  menuItem.push(hrMenuItem);
+}
+if(userRole == 'Employee') {
+  menuItem.push(employeeItem);
+}
+export default menuItem;
