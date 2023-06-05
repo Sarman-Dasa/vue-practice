672147d0dec8343a3e33b582e6ae640b6ea1c28c
Sarman-Dasa <sarmand@zignuts.com>
2023-06-05 19:01:56 +0530

    register new state

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,7 @@ import Vuex from 'vuex'
 import app from './app'
 import appConfig from './app-config'
 import verticalMenu from './vertical-menu'
+import myState from './myState'
 
 Vue.use(Vuex)
 
@@ -13,6 +14,7 @@ export default new Vuex.Store({
     app,
     appConfig,
     verticalMenu,
+    myState,
   },
   strict: process.env.DEV,
 })
